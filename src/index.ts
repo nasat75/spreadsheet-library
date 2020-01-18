ffb75c513c23c7b25b9d3079df1234d70a9b3ec2
Asato Nago <nago@tam-bourine.co.jp>
2020-01-18 11:43:04 +0900

    build: add yarn.lock

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,10 @@
 // import './app'
-import {getTitleName, getISBN, generateURL, throwAPI} from './app'
+import {getBookInfo} from './app'
 
 declare const global: {
   [x: string]: any
 }
 
-global.getTitleName = () => {
-  return getTitleName()
+global.getBookInfo = () => {
+  return getBookInfo();
 }
-
-global.getISBN = (title: string) => {
-  return getISBN(title)
-}
-
-global.generateURL = (searchType: string, args: string) => {
-  return generateURL(searchType, args)
-}
-
-global.throwAPI = (apiUrl: string) => {
-  return throwAPI(apiUrl)
-}
-
-
